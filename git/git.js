9122c1aaa08bfba294c4bd64572302e455f81ffe
adeeldev105 <adeel.dev@pk.see.biz>
2021-03-25 17:10:38 +0500

    cmmnt

---
+++ git/git.js
@@ -0,0 +1,12 @@
+function addition(total,value)
+{
+    return total+value;
+}
+function myFunction()
+{
+    var nums=[1,2,3,4,5,6,7,8,9];
+
+    var sum=nums.reduce(addition);
+    console.log("Sum : ",sum);
+}
+myFunction();
